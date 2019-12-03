bb1e67b480276e637ede5b738cdf4e5920acd7f5
Valerii Vasin <vvasin@groupon.com>
2019-12-03 02:39:49 +0100

    feat: solve 2.2

--- 2019/2/solution.js
+++ 2019/2/solution.js
@@ -54,13 +54,34 @@ console.assert(
   'example'
 )
 
-function solve() {
-  input[1] = 12;
-  input[2] = 2;
+function solve(input, { noun, verb }) {
+  const copy = [...input];
+  copy[1] = noun;
+  copy[2] = verb;
 
-  const programm = run(input);
+  const programm = run(copy);
 
   return programm[0];
 }
 
-console.log('answer: ', solve());
+console.log('answer part #1:', solve(input, { noun: 12, verb: 2 }));
+console.assert(solve(input, { noun: 12, verb: 2 }) === 4945026, 'answer part #1');
+
+function solveSecond() {
+  const EXPECTED = 19690720;
+
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const result = solve(input, { noun, verb });
+
+      if (result === EXPECTED) {
+        return noun * 100 + verb;
+      }
+    }
+  }
+
+  return -1;
+}
+
+console.log('answer part #2:', solveSecond());
+console.assert(solveSecond() === 5296, 'answer part #2');
