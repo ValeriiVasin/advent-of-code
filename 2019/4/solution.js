ba218e3c0b279375beed4f5d7fd99d4a4e002de2
Valerii Vasin <vvasin@groupon.com>
2019-12-04 11:01:16 +0100

    feat: solve #4.2

--- 2019/4/solution.js
+++ 2019/4/solution.js
@@ -4,6 +4,42 @@ function toDigits(value) {
     .map(_ => Number(_));
 }
 
+function isIncreasing(digits) {
+  let prev = digits[0];
+  for (let i = 1; i < digits.length; i++) {
+    const current = digits[i];
+    if (current < prev) {
+      return false;
+    }
+
+    prev = current;
+  }
+
+  return true;
+}
+
+function toGroups(digits) {
+  let prev = digits[0];
+  const groups = [];
+  let groupLength = 1;
+  for (let i = 1; i < digits.length; i++) {
+    const digit = digits[i];
+
+    if (digit === prev) {
+      groupLength++;
+      continue;
+    }
+
+    groups.push(groupLength);
+    groupLength = 1;
+    prev = digit;
+  }
+
+  groups.push(groupLength);
+
+  return groups;
+}
+
 function isPassword(value) {
   const digits = toDigits(value);
 
@@ -11,28 +47,32 @@ function isPassword(value) {
     return false;
   }
 
-  let havingTwoAdjacent = false;
-  let prev = digits[0];
-  for (const digit of digits.slice(1)) {
-    if (digit === prev) {
-      havingTwoAdjacent = true;
-    }
-
-    if (digit < prev) {
-      return false;
-    }
-
-    prev = digit;
+  if (!isIncreasing(digits)) {
+    return false;
   }
 
-  return havingTwoAdjacent;
+  return toGroups(digits).some(length => length > 1);
 }
 
-function solution(from, to) {
+function isPasswordImproved(value) {
+  const digits = toDigits(value);
+
+  if (digits.length !== 6) {
+    return false;
+  }
+
+  if (!isIncreasing(digits)) {
+    return false;
+  }
+
+  return toGroups(digits).includes(2);
+}
+
+function solution(from, to, check) {
   let count = 0;
 
   for (let i = from; i <= to; i++) {
-    if (isPassword(i)) {
+    if (check(i)) {
       count++;
     }
   }
@@ -40,9 +80,14 @@ function solution(from, to) {
   return count;
 }
 
-console.log('answer #1', solution(125730, 579381));
+console.log('answer #4.1', solution(125730, 579381, isPassword));
+console.log('answer #4.2', solution(125730, 579381, isPasswordImproved));
 
 module.exports = {
+  isIncreasing,
   toDigits,
   isPassword,
+  isPasswordImproved,
+  toGroups,
+  solution,
 };
--- 2019/4/solution.spec.js
+++ 2019/4/solution.spec.js
@@ -1,19 +1,64 @@
-const { isPassword, toDigits } = require('./solution');
+const {
+  isPassword,
+  isPasswordImproved,
+  isIncreasing,
+  toDigits,
+  toGroups,
+} = require('./solution');
 
-test('to digits', () => {
-  expect(toDigits(325)).toEqual([3, 2, 5]);
+describe('to digits', () => {
+  it('converts value to digits', () => {
+    expect(toDigits(325)).toEqual([3, 2, 5]);
+  });
 });
 
-test('valid', () => {
-  expect(isPassword(111123)).toBe(true);
-  expect(isPassword(135579)).toBe(true);
+describe('isIncreasing', () => {
+  it('increasing only values', () => {
+    expect(isIncreasing([1, 2, 3])).toBe(true);
+  });
+
+  it('increasing or equal values', () => {
+    expect(isIncreasing([1, 2, 2, 3])).toBe(true);
+  });
+
+  it('decreasing', () => {
+    expect(isIncreasing([1, 2, 2, 1])).toBe(false);
+  });
 });
 
-test('valid input test data', () => {
-  expect(isPassword(111111)).toBe(true);
+describe('grouping', () => {
+  it('proper group lengths', () => {
+    expect(toGroups([1, 2, 3])).toEqual([1, 1, 1]);
+    expect(toGroups([1, 2, 2, 3])).toEqual([1, 2, 1]);
+  });
 });
 
-test('not valid input test data', () => {
-  expect(isPassword(223450)).toBe(false);
-  expect(isPassword(123789)).toBe(false);
+describe('part #1', () => {
+  it('valid', () => {
+    expect(isPassword(111123)).toBe(true);
+    expect(isPassword(135579)).toBe(true);
+  });
+
+  it('valid input test data', () => {
+    expect(isPassword(111111)).toBe(true);
+  });
+
+  it('not valid input test data', () => {
+    expect(isPassword(223450)).toBe(false);
+    expect(isPassword(123789)).toBe(false);
+  });
+});
+
+describe('part #2', () => {
+  it('validnever decrease and all repeated digits are exactly two digits long', () => {
+    expect(isPasswordImproved(112233)).toBe(true);
+  });
+
+  it('not having exactly 2-digits long group', () => {
+    expect(isPasswordImproved(123444)).toBe(false);
+  });
+
+  it('having two repeated groups, but one is exactly 2-digits long', () => {
+    expect(isPasswordImproved(111122)).toBe(true);
+  });
 });
