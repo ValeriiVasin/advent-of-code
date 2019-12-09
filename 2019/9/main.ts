3a8e0a98ec7297f8694818c73c91a5faa70aa72b
Valerii Vasin <vvasin@groupon.com>
2019-12-09 14:43:22 +0100

    solve #9.2

--- 2019/9/main.ts
+++ 2019/9/main.ts
@@ -2,6 +2,15 @@ import { program } from './input';
 import { run } from '../5/lib';
 
 (async function main() {
-  const { output } = await run({ program: [...program], input: [1] });
-  console.log('answer #9.1:', output[0]);
+  const { output: outputOne } = await run({
+    program: [...program],
+    input: [1],
+  });
+  console.log('answer #9.1:', outputOne[0]);
+
+  const { output: outputTwo } = await run({
+    program: [...program],
+    input: [2],
+  });
+  console.log('answer #9.2:', outputTwo[0]);
 })();
