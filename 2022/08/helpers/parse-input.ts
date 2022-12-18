38302c3ec3cf900463ad67f1faa80c91f5667e52
Valerii Vasin <valerii.vasin@gmail.com>
2022-12-18 03:13:38 +0100

    feat: get result #8.1

--- 2022/08/helpers/parse-input.ts
+++ 2022/08/helpers/parse-input.ts
@@ -1,5 +1,6 @@
 import { getLines } from '../../helpers/get-lines';
+import type { Grid } from '../types';
 
-export function parseInput(content: string): Array<Array<number>> {
+export function parseInput(content: string): Grid<number> {
   return getLines(content).map((line) => line.split('').map(Number));
 }
